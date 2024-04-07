29155729750f019dc0259aa2278b002b6faa0b2b
“MingWei <2570016347@qq.com>
2024-04-07 22:28:55 +0800

    add Serilog

--- AI.DeliciousFood.Web.Client/Controllers/HomeController.cs
+++ AI.DeliciousFood.Web.Client/Controllers/HomeController.cs
@@ -2,27 +2,27 @@
 using AI.DeliciousFood.Web.Client.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 using System.Diagnostics;
 
 namespace AI.DeliciousFood.Web.Client.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
         private readonly SignInManager<FoodUser> _signInManager;
         private readonly UserManager<FoodUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger,
+        public HomeController(
             SignInManager<FoodUser> signInManager,
             UserManager<FoodUser> userManager)
         {
-            _logger = logger;
             this._signInManager = signInManager;
             this._userManager = userManager;
         }
 
         public IActionResult Index()
         {
+            Log.Error("Hello World");
             return View();
         }
 
