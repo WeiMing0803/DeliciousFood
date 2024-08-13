dc60ac4084bc9f87730d889380060b9af6dbe03c
v-weimin@handshakes.com.sg <N7NJIHFSLlHtFC6f>
2024-08-13 17:22:40 +0800

    开发后端管理系统

---
+++ AI.DeliciousFood.Web.Manage/Controllers/AccountLogonController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AI.DeliciousFood.Web.Manage.Controllers
+{
+    public class AccountLogonController : Controller
+    {
+        [HttpGet]
+        public IActionResult LogOn()
+        {
+            return View();
+        }
+    }
+}
