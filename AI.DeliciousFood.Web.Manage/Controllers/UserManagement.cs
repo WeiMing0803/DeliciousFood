1aba3a85305a45f058b8286cfb4482e326018137
v-weimin@handshakes.com.sg <N7NJIHFSLlHtFC6f>
2024-09-04 17:46:03 +0800

    配置bootstrap-multitabs

---
+++ AI.DeliciousFood.Web.Manage/Controllers/UserManagement.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AI.DeliciousFood.Web.Manage.Controllers;
+
+public class UserManagement : Controller
+{
+    public IActionResult UserList(int page = 1, int item = 10)
+    {
+        return View();
+    }
+}
