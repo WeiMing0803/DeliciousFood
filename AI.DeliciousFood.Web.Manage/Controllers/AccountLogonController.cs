4a24e348cc456333b215de2df0f030c869bb154b
v-weimin@handshakes.com.sg <N7NJIHFSLlHtFC6f>
2024-11-12 17:06:19 +0800

    编写前端登录逻辑

--- AI.DeliciousFood.Web.Manage/Controllers/AccountLogonController.cs
+++ AI.DeliciousFood.Web.Manage/Controllers/AccountLogonController.cs
@@ -1,13 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AI.DeliciousFood.Core.Common.ManageModel.UserManager;
+using Microsoft.AspNetCore.Mvc;
 
-namespace AI.DeliciousFood.Web.Manage.Controllers
+namespace AI.DeliciousFood.Web.Manage.Controllers;
+
+public class AccountLogonController : Controller
 {
-    public class AccountLogonController : Controller
+    [HttpGet]
+    public IActionResult LogOn()
     {
-        [HttpGet]
-        public IActionResult LogOn()
-        {
-            return View();
-        }
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult LogOn(LogOnModel model) 
+    {
+        return Json(new LogOnResponse(StatusCodes.Status200OK, "success"));
     }
 }
